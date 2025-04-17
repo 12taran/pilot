6b5a656d368dc21342fefb6bb5ac0593cc3922c1
Vishal Aggarwal <007vishal.aggarwal@gmail.com>
2025-04-17 17:50:57 +0530

    Error User Update not working

--- Backend/Models/user.model.js
+++ Backend/Models/user.model.js
@@ -1,11 +1,22 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  phoneNumber: {
-    type: Number,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: false,
+    },
+    address: {
+      type: String,
+      required: false,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 export const User = mongoose.model("User", userSchema);
